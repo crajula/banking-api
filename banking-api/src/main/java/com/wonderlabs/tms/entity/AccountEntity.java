dcbc41444264ae8e77c2a141b546444bfdfd20ce
crajula <rajula.za@gmail.com>
2023-02-26 16:20:39 +0530

    Create entities initial commit

---
+++ banking-api/src/main/java/com/wonderlabs/tms/entity/AccountEntity.java
@@ -0,0 +1,28 @@
+package com.wonderlabs.tms.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+@AllArgsConstructor
+@Builder
+@Data
+public class AccountEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tutorial_generator")
+    private long id;
+
+    @Column(name = "title")
+    private String name;
+
+    @Column(name = "description")
+    private String surname;
+
+    @Column(name = "published")
+    private boolean mobileNumber;
+}
